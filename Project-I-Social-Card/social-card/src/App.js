b65922d14dbdd880d9bb57a6e6330b6619a89140
dgbailey <bailey.dust@gmail.com>
2019-03-18 16:05:43 -0700

    mvp and part of stretch

--- Project-I-Social-Card/social-card/src/App.js
+++ Project-I-Social-Card/social-card/src/App.js
@@ -2,13 +2,13 @@ import React from 'react';
 import './App.css';
 import HeaderContainer from './components/HeaderComponents/HeaderContainer';
 import CardContainer from './components/CardComponents/CardContainer';
-
+import FooterContainer from './components/FooterComponents/Footer';
 const App = () => {
   return (
     <div id='largeCont'>
       <HeaderContainer/>
       <CardContainer/>
-
+      <FooterContainer/>
      
     </div>
   );
