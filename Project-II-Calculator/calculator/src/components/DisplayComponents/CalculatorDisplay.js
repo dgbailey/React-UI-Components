4fc973cfb95b411b094073d2486fc18fb61a0fe5
dgbailey <bailey.dust@gmail.com>
2019-03-19 16:10:30 -0700

    MVP

--- Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
+++ Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
@@ -1,2 +1,19 @@
 import React from 'react';
 import './Display.css';
+import NonOperatorContainer from '../ButtonComponents/NonOperatorContainer';
+import OperatorContainer from '../ButtonComponents/OperatorContainer';
+
+
+const CalculatorDisplay = () =>{
+
+   return (
+   <div class= 'global-display'>
+        <div class='io-display'><span class='num'>0</span></div>
+        <div class='all-btns'>
+            <NonOperatorContainer/>
+            {/* <OperatorContainer/> */}
+        </div>
+    </div>);
+}
+
+export default CalculatorDisplay;
